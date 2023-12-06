dc2b5b3fd8613977f39f19a49e75c2cc1e2f21fd
Chigozie Nri <chigozie@replicate.com>
2023-12-06 12:12:21 +0000

    Use viridis, import from main

--- predict.py
+++ predict.py
@@ -6,7 +6,10 @@ import shutil
 import subprocess
 import tempfile
 
+import cv2
 from cog import BasePredictor, Input, Path
+
+from main import densepose
 
 
 class Predictor(BasePredictor):
@@ -23,12 +26,14 @@ class Predictor(BasePredictor):
         in_path = os.path.join(tempdir, "image.png")
         out_path = os.path.join(tempdir, "image_dp_segm.png")
         shutil.copy(image, in_path)
-        tempdir_contents = set(os.listdir(tempdir))
-        subprocess.check_output(
-            f"python /detectron2/projects/DensePose/apply_net.py show /detectron2/projects/DensePose/configs/densepose_rcnn_R_50_FPN_s1x.yaml /model_final_162be9.pkl {in_path} dp_segm --output {out_path}",
-            shell=True,
-        )
+        # tempdir_contents = set(os.listdir(tempdir))
+        out = densepose(in_path)
+        cv2.imwrite(out_path, out)
+        # subprocess.check_output(
+        #     f"python /detectron2/projects/DensePose/apply_net.py show /detectron2/projects/DensePose/configs/densepose_rcnn_R_50_FPN_s1x.yaml /model_final_162be9.pkl {in_path} dp_segm --output {out_path}",
+        #     shell=True,
+        # )
         # Weird workaround because the output file is not named correctly, it has a .0001 inserted before the extension
-        filename = list(set(os.listdir(tempdir)).difference(tempdir_contents))[0]
-        out_path = os.path.join(tempdir, filename)
+        # filename = list(set(os.listdir(tempdir)).difference(tempdir_contents))[0]
+        # out_path = os.path.join(tempdir, filename)
         return Path(out_path)
